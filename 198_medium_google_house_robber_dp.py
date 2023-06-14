2d50739601bbdb4715325a9f7069a96f7e2380ab
kris-randen <kris.randen@gmail.com>
2023-06-14 16:06:34 +0530

    Solved house robber correctly using DP. Also solved longest palindromic substring by just considering 2n - 1 centers instead of quadratic combinations of all i, j positions.

---
+++ 198_medium_google_house_robber_dp.py
@@ -0,0 +1,48 @@
+"""
+198. House Robber
+Medium
+18K
+341
+company
+Cisco
+company
+Amazon
+company
+Google
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+"""
+
+def max_loot(sums):
+    if not sums:
+        return 0
+    if len(sums) == 1:
+        return sums[0]
+    if len(sums) == 2:
+        return max(sums[0], sums[1])
+
+    n = len(sums)
+    loot = []
+    loot[0] = sums[0]
+    loot[1] = max(sums[0], sums[1])
+
+    for i in range(3, n):
+        loot[i] = max(loot[i - 1], loot[i - 2] + sums[i])
+
+    return loot[n - 1]
--- 5_medium_google_longest_pal_substring_string.py
+++ 5_medium_google_longest_pal_substring_string.py
@@ -63,3 +63,54 @@ def longest_palindrome(s):
                 table[i][j] = True
 
     return s[start:start + max_len]
+
+# There are 2n - 1 centers for palindromes in any string of length n
+# From each center starting from the middle center we start going out
+# we find the longest palindrome and recurse on the left and right
+# halves.
+
+def index(c):
+    return [c//2] if c % 2 == 0 else [c//2, c//2 + 1]
+
+def max_palindrome(s):
+    n = len(s)
+    if n < 2:
+        return s
+    centers = [_ for _ in range(2*n - 1)]
+    table = [0 for _ in centers]
+    start = 0
+    max_len = 1
+    start_max = [start, max_len]
+    for center in centers:
+        centred_palindrome(s, center, table, start_max)
+
+    return s[start_max[0]:start_max[0] + start_max[1]]
+
+
+def centred_palindrome(s, c, table, start_max):
+    i = 0
+    n = len(s)
+    indices = index(c)
+    if len(indices) == 1:
+        m = index(c)[0]
+        while m - i >= 0 and m + i < n and s[m - i] == s[m + i]:
+            length = 2 * i + 1
+            table[c] = length
+            if start_max[1] < length:
+                start_max[1] = length
+                start_max[0] = m - i
+            i += 1
+    else:
+        l, r = indices[0], indices[1]
+        while l - i >= 0 and r + i < n and s[l - i] == s[r + i]:
+            length = 2 * (i + 1)
+            table[c] = length
+            if start_max[1] < length:
+                start_max[1] = length
+                start_max[0] = l - i
+            i += 1
+
+
+
+if __name__ == '__main__':
+    print(max_palindrome("caba"))
