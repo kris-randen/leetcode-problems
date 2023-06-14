d4ea292ed86db2303d204a9b9a5a310446ed8a8a
kris-randen <kris.randen@gmail.com>
2023-06-14 16:49:38 +0530

    Was able to significantly improve the performance by removing table. Since we are directly updating the start index and max length we don't need to store lengths in the table. Also we only check for centered palindromes for centers that are at least at max_length // 2 radius or more from both the ends.

--- 5_medium_google_longest_pal_substring_string.py
+++ 5_medium_google_longest_pal_substring_string.py
@@ -70,47 +70,38 @@ def longest_palindrome(s):
 # halves.
 
 def index(c):
-    return [c//2] if c % 2 == 0 else [c//2, c//2 + 1]
+    return [c//2, c//2 + 1] if (c + 1) % 2 == 0 else [c//2]
+
+def update_start_max(start_max, index, length):
+    start, max_length = start_max
+    if length > max_length:
+        start_max[0] = index
+        start_max[1] = length
 
 def max_palindrome(s):
     n = len(s)
     if n < 2:
         return s
     centers = [_ for _ in range(2*n - 1)]
-    table = [0 for _ in centers]
-    start = 0
-    max_len = 1
-    start_max = [start, max_len]
+    start, max_length = 0, 1
+    start_max = [start, max_length]
+
     for center in centers:
-        centred_palindrome(s, center, table, start_max)
+        centred_palindrome(s, center, start_max)
+    start, max_length = start_max
+    return s[start:start + max_length]
 
-    return s[start_max[0]:start_max[0] + start_max[1]]
-
-
-def centred_palindrome(s, c, table, start_max):
-    i = 0
-    n = len(s)
-    indices = index(c)
-    if len(indices) == 1:
-        m = index(c)[0]
-        while m - i >= 0 and m + i < n and s[m - i] == s[m + i]:
-            length = 2 * i + 1
-            table[c] = length
-            if start_max[1] < length:
-                start_max[1] = length
-                start_max[0] = m - i
-            i += 1
-    else:
-        l, r = indices[0], indices[1]
-        while l - i >= 0 and r + i < n and s[l - i] == s[r + i]:
-            length = 2 * (i + 1)
-            table[c] = length
-            if start_max[1] < length:
-                start_max[1] = length
-                start_max[0] = l - i
-            i += 1
+def centred_palindrome(s, c, start_max):
+    i, n, indices, even, radius = 0, len(s), index(c), (c + 1) % 2 == 0, start_max[1] // 2
+    l, r = (indices[0], indices[1]) if even else (indices[0], indices[0])
+    if l - radius < 0 or r + radius > n - 1:
+        return
+    while l - i >= 0 and r + i < n and s[l - i] == s[r + i]:
+        length = (2 * i + 1) if not even else 2*(i + 1)
+        update_start_max(start_max, l - i, length)
+        i += 1
 
 
 
 if __name__ == '__main__':
-    print(max_palindrome("caba"))
+    print(max_palindrome("cbbd"))
