32fbab63397adcffc10a013ecb1c9ff319420ab1
kris-randen <kris.randen@gmail.com>
2023-06-19 16:54:43 +0530

    Refactored 144 Easy Preorder Traversal into using a stack (from collections import deque) correctly. Made it almost medium - hard. But not too much improvement in the performance because of not relying on function call stacks. Runtime 42 ms Beats 73.98% Memory 16.5 MB Beats 28.1%

--- Binary Search Tree/Traversal/144_easy_preorder_bst.py
+++ Binary Search Tree/Traversal/144_easy_preorder_bst.py
@@ -28,11 +28,29 @@ Input: root = [1]
 Output: [1]
 """
 
+from collections import deque
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+def preorder_stack(node, path):
+    if not node: return
+    de, visited = deque([node]), set()
+    while len(de) != 0:
+        p = de[-1]
+        if p not in visited:
+            path.append(p.val); visited.add(p)
+        else: p = de.pop()
+        if p.left and p.left not in visited:
+            de.append(p.left)
+        if not p.left or p.left in visited:
+            if p.right and p.right not in visited:
+                de.append(p.right)
+            else: continue
+
 
 def preorder(node, path):
     if not node: return
