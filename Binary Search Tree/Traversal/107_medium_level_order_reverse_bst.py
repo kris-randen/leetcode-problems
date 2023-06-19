1852ae710f5ac2944e5cb1bfb96f24432a433a24
kris-randen <kris.randen@gmail.com>
2023-06-19 20:36:43 +0530

    Runtime 38 ms Beats 91.68% Memory 16.7 MB Beats 57.56%

---
+++ Binary Search Tree/Traversal/107_medium_level_order_reverse_bst.py
@@ -0,0 +1,36 @@
+"""
+107. Binary Tree Level Order Traversal II
+Medium
+4.4K
+310
+company
+Amazon
+Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
+
+
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[15,7],[9,20],[3]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+
+"""
+
+def level_order_reverse(node):
+    if not node: return []
+    q, path, cs, vs = [[node]], [], [], []
+    for ps in q:
+        for p in ps:
+            if p:
+                vs += [p.val]; cs += [p.left, p.right]
+        if cs: path += [vs]; q += [cs]; cs = []; vs = []
+    return reversed(path)
