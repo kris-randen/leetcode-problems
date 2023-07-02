b32a04811635f7f5c0d33996818ad17d306eb6d1
kris-randen <kris.randen@gmail.com>
2023-07-02 20:22:14 +0530

    Solved 189 Rotate Array. Performance Runtime 212 ms Beats 91.5% Memory 27.9 MB Beats 10.15%

---
+++ 01 Arrays and Sorting/189_medium_lc_150_rotate_array.py
@@ -0,0 +1,34 @@
+"""
+189. Rotate Array
+Medium
+14.8K
+1.7K
+Companies
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+"""
+
+def shift(v):
+    v.insert(0, v.pop())
+
+def rotate(v, r):
+    if len(v) < 2: return
+    for i in range(r):
+        shift(v)
